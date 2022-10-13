79396bafdcda560dd08f8fd9f519df042d41f52d
Fabian Schmauder <fabian.schmauder@gmail.com>
2022-10-13 11:36:59 +0200

    Add Web instance

--- main.tf
+++ main.tf
@@ -11,26 +11,3 @@ terraform {
 provider "aws" {
   region = "us-west-2"
 }
-
-resource "aws_vpc" "myvpc" {
-  cidr_block = "10.0.0.0/16"
-  tags = {
-    Name = "My VPC"
-  }
-}
-
-resource "aws_subnet" "private_subnet_1" {
-  vpc_id     = aws_vpc.myvpc.id
-  cidr_block = "10.0.1.0/24"
-  tags = {
-    Name = "Private 1"
-  }
-}
-
-resource "aws_subnet" "private_subnet_2" {
-  vpc_id     = aws_vpc.myvpc.id
-  cidr_block = "10.0.2.0/24"
-  tags = {
-    Name = "Private 2"
-  }
-}
